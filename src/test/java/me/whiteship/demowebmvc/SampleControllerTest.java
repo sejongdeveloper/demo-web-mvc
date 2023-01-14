e61d5a315acf45c5dd6c76057ecdefd846d87484
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-14 23:55:39 +0900

    핸들러 메소드 8부 @SessionAttributes

--- src/test/java/me/whiteship/demowebmvc/SampleControllerTest.java
+++ src/test/java/me/whiteship/demowebmvc/SampleControllerTest.java
@@ -3,6 +3,7 @@ package me.whiteship.demowebmvc;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.web.servlet.ModelAndView;
@@ -22,12 +23,14 @@ public class SampleControllerTest {
 
     @Test
     public void eventForm() throws Exception {
-        mockMvc.perform(get("/events/form"))
+        MockHttpServletRequest request = mockMvc.perform(get("/events/form"))
                 .andDo(print())
                 .andExpect(view().name("events/form"))
                 .andExpect(model().attributeExists("event"))
                 .andExpect(request().sessionAttribute("event", notNullValue()))
-        ;
+                .andReturn().getRequest();
+        Object event = request.getSession().getAttribute("event");
+        System.out.println(event);
     }
 
     @Test
