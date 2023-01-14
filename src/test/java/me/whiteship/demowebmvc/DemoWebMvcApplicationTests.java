6008764779ed0bd1f726131892e2faf4c529ce7c
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-14 16:29:17 +0900

    요청 맵핑하기 1부 HTTP Method

---
+++ src/test/java/me/whiteship/demowebmvc/DemoWebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package me.whiteship.demowebmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/me/whiteship/demowebmvc/SampleControllerTest.java
@@ -0,0 +1,27 @@
+package me.whiteship.demowebmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class SampleControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void helloTest() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello"))
+        ;
+    }
+
+}
