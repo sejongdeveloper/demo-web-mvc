2a5e9482d4e63825e362e3adb6bd8d88e13cc780
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-14 16:52:33 +0900

    요청 맵핑하기 1부 HTTP Method

--- src/main/java/me/whiteship/demowebmvc/SampleController.java
+++ src/main/java/me/whiteship/demowebmvc/SampleController.java
@@ -1,13 +1,15 @@
 package me.whiteship.demowebmvc;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping(method = RequestMethod.GET)
 public class SampleController {
 
-    @GetMapping(value = "/hello")
+    @RequestMapping(value = "/hello")
     @ResponseBody
     public String hello() {
         return "hello";
