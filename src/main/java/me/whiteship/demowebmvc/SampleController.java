f0172a00ffc6325012935ad4f6717a0ad9655086
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-14 17:36:47 +0900

    요청 맵핑하기 3부 미디어 타입

--- src/main/java/me/whiteship/demowebmvc/SampleController.java
+++ src/main/java/me/whiteship/demowebmvc/SampleController.java
@@ -3,9 +3,11 @@ package me.whiteship.demowebmvc;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping(consumes = MediaType.APPLICATION_XML_VALUE)
 public class SampleController {
 
     @GetMapping(
