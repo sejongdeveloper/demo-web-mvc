a16eecf902ff2f2924984aec8e6d3e8f3de76777
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-14 20:51:09 +0900

    핸들러 메소드 2부 URI 패턴

--- src/main/java/me/whiteship/demowebmvc/SampleController.java
+++ src/main/java/me/whiteship/demowebmvc/SampleController.java
@@ -4,6 +4,7 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.MatrixVariable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,28 +13,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class SampleController {
 
-    @GetMapping("/events")
-    @ResponseBody
-    public String events() {
-        return "events";
-    }
-
     @GetMapping("/events/{id}")
     @ResponseBody
-    public String getAnEvents(@PathVariable int id) {
-        return "event";
+    public Event getEvent(@PathVariable Integer id, @MatrixVariable String name) {
+        Event event = new Event();
+        event.setId(id);
+        event.setName(name);
+        return event;
     }
-
-    @DeleteMapping("/events/{id}")
-    @ResponseBody
-    public String removeAnEvents(@PathVariable int id) {
-        return "event";
-    }
-
-    @GetHelloMapping
-    @ResponseBody
-    public String hello() {
-        return "hello";
-    }
-
 }
--- src/test/java/me/whiteship/demowebmvc/SampleControllerTest.java
+++ src/test/java/me/whiteship/demowebmvc/SampleControllerTest.java
@@ -3,11 +3,11 @@ package me.whiteship.demowebmvc;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
@@ -16,53 +16,12 @@ public class SampleControllerTest {
     MockMvc mockMvc;
 
     @Test
-    public void deleteEvent() throws Exception {
-        mockMvc.perform(delete("/events/1"))
-                .andExpect(status().isOk());
-        mockMvc.perform(delete("/events/2"))
-                .andExpect(status().isOk());
-        mockMvc.perform(delete("/events/3"))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void createEvent() throws Exception {
-        mockMvc.perform(post("/events")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void updateEvent() throws Exception {
-        mockMvc.perform(put("/events")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void helloTest() throws Exception {
-        mockMvc.perform(options("/hello"))
+    public void getEvent() throws Exception {
+        mockMvc.perform(get("/events/1;name=keesun"))
                 .andDo(print())
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("id").value(1))
         ;
-    }
-
-    @Test
-    public void getEvents() throws Exception {
-        mockMvc.perform(get("/events"))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void getEventsWithId() throws Exception {
-        mockMvc.perform(get("/events/1"))
-                .andExpect(status().isOk());
-        mockMvc.perform(get("/events/2"))
-                .andExpect(status().isOk());
-        mockMvc.perform(get("/events/3"))
-                .andExpect(status().isOk());
     }
 
 }
