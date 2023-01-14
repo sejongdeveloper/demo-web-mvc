5dac1a0596ab304decd9ed6fcc259b67786eebd7
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-14 17:32:47 +0900

    요청 맵핑하기 3부 미디어 타입

--- src/main/java/me/whiteship/demowebmvc/SampleController.java
+++ src/main/java/me/whiteship/demowebmvc/SampleController.java
@@ -2,13 +2,16 @@ package me.whiteship.demowebmvc;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SampleController {
 
-    @RequestMapping(value = "/hello", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @GetMapping(
+            value = "/hello",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.TEXT_PLAIN_VALUE)
     @ResponseBody
     public String hello() {
         return "hello";
--- src/test/java/me/whiteship/demowebmvc/SampleControllerTest.java
+++ src/test/java/me/whiteship/demowebmvc/SampleControllerTest.java
@@ -3,6 +3,7 @@ package me.whiteship.demowebmvc;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -16,9 +17,11 @@ public class SampleControllerTest {
 
     @Test
     public void helloTest() throws Exception {
-        mockMvc.perform(get("/hello"))
+        mockMvc.perform(get("/hello")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
-                .andExpect(status().isUnsupportedMediaType())
+                .andExpect(status().isNotAcceptable())
         ;
     }
 
