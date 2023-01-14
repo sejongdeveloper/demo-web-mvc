56adfcbcb56e8c1b237a73fe0f14225c18d6abc8
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-14 17:06:17 +0900

    요청 맵핑하기 2부 URI 패턴

--- src/main/java/me/whiteship/demowebmvc/SampleController.java
+++ src/main/java/me/whiteship/demowebmvc/SampleController.java
@@ -2,12 +2,14 @@ package me.whiteship.demowebmvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/hello")
 public class SampleController {
 
-    @GetMapping("/hello/**")
+    @GetMapping("/**")
     @ResponseBody
     public String hello() {
         return "hello";
