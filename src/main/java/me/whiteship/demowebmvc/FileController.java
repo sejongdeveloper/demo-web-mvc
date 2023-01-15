2bc8530295af6d3bc8b378fd7cb671fceb271a96
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-15 10:32:24 +0900

    핸들러 메소드 14부 ResponseEntity

--- src/main/java/me/whiteship/demowebmvc/FileController.java
+++ src/main/java/me/whiteship/demowebmvc/FileController.java
@@ -1,15 +1,27 @@
 package me.whiteship.demowebmvc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
+import java.io.IOException;
+
 @Controller
 public class FileController {
+
+    @Autowired
+    private ResourceLoader resourceLoader;
 
     @GetMapping("/file")
     public String fileUploadForm(Model model) {
@@ -25,5 +37,16 @@ public class FileController {
         String message = file.getOriginalFilename() + " is uploaded";
         attributes.addFlashAttribute("message", message);
         return "redirect:/file";
+    }
+
+    @GetMapping("/file/{filename}")
+    public ResponseEntity fileDownload(@PathVariable String filename) throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:" + filename);
+        File file = resource.getFile();
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .header(HttpHeaders.CONTENT_TYPE, "image/png")
+                .header(HttpHeaders.CONTENT_LENGTH, file.length() + "")
+                .body(resource);
     }
 }
