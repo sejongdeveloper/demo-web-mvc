80dc79f2d61dcdac656e9b56612f139e4bfb9575
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-15 10:52:58 +0900

    핸들러 메소드 15부 @RequestBody & HttpEntity

--- src/main/java/me/whiteship/demowebmvc/EventApi.java
+++ src/main/java/me/whiteship/demowebmvc/EventApi.java
@@ -1,7 +1,8 @@
 package me.whiteship.demowebmvc;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class EventApi {
 
     @PostMapping
-    public Event createEvent(@RequestBody Event event) {
+    public Event createEvent(HttpEntity<Event> request) {
         // save event
-        return event;
+        MediaType contentType = request.getHeaders().getContentType();
+        System.out.println(contentType);
+        return request.getBody();
     }
 }
