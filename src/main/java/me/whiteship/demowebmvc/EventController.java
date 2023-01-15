abde794ae79ef609099d8e41e3dc859b06d07c47
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-15 11:19:33 +0900

    모델 @ModelAttribute

--- src/main/java/me/whiteship/demowebmvc/EventController.java
+++ src/main/java/me/whiteship/demowebmvc/EventController.java
@@ -19,6 +19,15 @@ import java.util.List;
 @Controller
 @SessionAttributes("event")
 public class EventController {
+//    @ModelAttribute
+//    public void categories(Model model) {
+//        model.addAttribute("categories", List.of("study", "seminar", "hobby", "social"));
+//    }
+
+    @ModelAttribute("categories")
+    public List<String> categories(Model model) {
+        return List.of("study", "seminar", "hobby", "social");
+    }
 
     @GetMapping("/events/form/name")
     public String eventsFormName(Model model) {
